41f76adac8008f2124dcb00616a8093f5afebaa2
HOPELESSNESSLYR <2984757450@qq.com>
2025-02-08 10:07:04 +0800

    alert-menu

---
+++ src/api/alert/alert.js
@@ -0,0 +1,44 @@
+import request from '@/utils/request'
+
+// 查询预警报警列表
+export function listAlert(query) {
+  return request({
+    url: '/alert/alert/list',
+    method: 'get',
+    params: query
+  })
+}
+
+// 查询预警报警详细
+export function getAlert(id) {
+  return request({
+    url: '/alert/alert/' + id,
+    method: 'get'
+  })
+}
+
+// 新增预警报警
+export function addAlert(data) {
+  return request({
+    url: '/alert/alert',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改预警报警
+export function updateAlert(data) {
+  return request({
+    url: '/alert/alert',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除预警报警
+export function delAlert(id) {
+  return request({
+    url: '/alert/alert/' + id,
+    method: 'delete'
+  })
+}
---
+++ src/views/fire/alert/index.vue
@@ -0,0 +1,337 @@
+<template>
+  <div class="app-container">
+    <el-form :model="queryParams" ref="queryForm" size="small" :inline="true" v-show="showSearch" label-width="68px">
+      <!-- <el-form-item label="${comment}" prop="fireId">
+        <el-input
+          v-model="queryParams.fireId"
+          placeholder="请输入${comment}"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item> -->
+      <el-form-item label="设备名称" prop="deviceName">
+        <el-input
+          v-model="queryParams.deviceName"
+          placeholder="请输入设备名称"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="设备型号" prop="deviceModel">
+        <el-input
+          v-model="queryParams.deviceModel"
+          placeholder="请输入设备型号"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="放置位置" prop="locate">
+        <el-input
+          v-model="queryParams.locate"
+          placeholder="请输入放置位置"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="预警记录" prop="alertRecord">
+        <el-input
+          v-model="queryParams.alertRecord"
+          placeholder="请输入预警记录"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="预警记录时间" prop="alertTime">
+        <el-date-picker clearable
+          v-model="queryParams.alertTime"
+          type="date"
+          value-format="yyyy-MM-dd"
+          placeholder="请选择预警记录时间">
+        </el-date-picker>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
+        <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-row :gutter="10" class="mb8">
+      <!-- <el-col :span="1.5">
+        <el-button
+          type="primary"
+          plain
+          icon="el-icon-plus"
+          size="mini"
+          @click="handleAdd"
+          v-hasPermi="['alert:alert:add']"
+        >新增</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="success"
+          plain
+          icon="el-icon-edit"
+          size="mini"
+          :disabled="single"
+          @click="handleUpdate"
+          v-hasPermi="['alert:alert:edit']"
+        >修改</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="danger"
+          plain
+          icon="el-icon-delete"
+          size="mini"
+          :disabled="multiple"
+          @click="handleDelete"
+          v-hasPermi="['alert:alert:remove']"
+        >删除</el-button>
+      </el-col> -->
+      <el-col :span="1.5">
+        <el-button
+          type="warning"
+          plain
+          icon="el-icon-download"
+          size="mini"
+          @click="handleExport"
+          v-hasPermi="['alert:alert:export']"
+        >导出</el-button>
+      </el-col>
+      <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
+    </el-row>
+
+    <el-table v-loading="loading" :data="alertList" @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="55" align="center" />
+      <el-table-column label="序号" type="index" align="center" prop="index">
+        <template slot-scope="scope">
+          <span>{{(queryParams.pageNum - 1) * queryParams.pageSize + scope.$index + 1}}</span>
+        </template>
+      </el-table-column>
+      <!-- <el-table-column label="预警主键" align="center" prop="id" />
+      <el-table-column label="${comment}" align="center" prop="fireId" /> -->
+      <el-table-column label="设备名称" align="center" prop="deviceName" />
+      <el-table-column label="设备型号" align="center" prop="deviceModel" />
+      <el-table-column label="放置位置" align="center" prop="locate" />
+      <el-table-column label="预警记录" align="center" prop="alertRecord" />
+      <el-table-column label="预警记录时间" align="center" prop="alertTime" width="180">
+        <!-- <template slot-scope="scope">
+          <span>{{ parseTime(scope.row.alertTime, '{y}-{m}-{d}') }}</span>
+        </template> -->
+      </el-table-column>
+      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            type="text"
+            icon="el-icon-edit"
+            @click="handleUpdate(scope.row)"
+            v-hasPermi="['alert:alert:edit']"
+          >修改</el-button>
+          <el-button
+            size="mini"
+            type="text"
+            icon="el-icon-delete"
+            @click="handleDelete(scope.row)"
+            v-hasPermi="['alert:alert:remove']"
+          >删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    
+    <pagination
+      v-show="total>0"
+      :total="total"
+      :page.sync="queryParams.pageNum"
+      :limit.sync="queryParams.pageSize"
+      @pagination="getList"
+    />
+
+    <!-- 添加或修改预警报警对话框 -->
+    <el-dialog :title="title" :visible.sync="open" width="500px" append-to-body>
+      <el-form ref="form" :model="form" :rules="rules" label-width="80px">
+        <!-- <el-form-item label="${comment}" prop="fireId">
+          <el-input v-model="form.fireId" placeholder="请输入${comment}" />
+        </el-form-item> -->
+        <el-form-item label="设备名称" prop="deviceName">
+          <el-input v-model="form.deviceName" placeholder="请输入设备名称" />
+        </el-form-item>
+        <el-form-item label="设备型号" prop="deviceModel">
+          <el-input v-model="form.deviceModel" placeholder="请输入设备型号" />
+        </el-form-item>
+        <el-form-item label="放置位置" prop="locate">
+          <el-input v-model="form.locate" placeholder="请输入放置位置" />
+        </el-form-item>
+        <el-form-item label="预警记录" prop="alertRecord">
+          <el-input v-model="form.alertRecord" placeholder="请输入预警记录" />
+        </el-form-item>
+        <el-form-item label="预警记录时间" prop="alertTime">
+          <el-date-picker clearable
+            v-model="form.alertTime"
+            type="date"
+            value-format="yyyy-MM-dd"
+            placeholder="请选择预警记录时间">
+          </el-date-picker>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="submitForm">确 定</el-button>
+        <el-button @click="cancel">取 消</el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import { listAlert, getAlert, delAlert, addAlert, updateAlert } from "@/api/alert/alert";
+
+export default {
+  name: "Alert",
+  data() {
+    return {
+      // 遮罩层
+      loading: true,
+      // 选中数组
+      ids: [],
+      // 非单个禁用
+      single: true,
+      // 非多个禁用
+      multiple: true,
+      // 显示搜索条件
+      showSearch: true,
+      // 总条数
+      total: 0,
+      // 预警报警表格数据
+      alertList: [],
+      // 弹出层标题
+      title: "",
+      // 是否显示弹出层
+      open: false,
+      // 查询参数
+      queryParams: {
+        pageNum: 1,
+        pageSize: 10,
+        fireId: null,
+        deviceName: null,
+        deviceModel: null,
+        locate: null,
+        alertRecord: null,
+        alertTime: null,
+      },
+      // 表单参数
+      form: {},
+      // 表单校验
+      rules: {
+        fireId: [
+          { required: true, message: "$comment不能为空", trigger: "blur" }
+        ],
+      },
+      index:"",
+    };
+  },
+  created() {
+    this.getList();
+  },
+  methods: {
+    /** 查询预警报警列表 */
+    getList() {
+      this.loading = true;
+      listAlert(this.queryParams).then(response => {
+        this.alertList = response.rows;
+        this.total = response.total;
+        this.loading = false;
+      });
+    },
+    // 取消按钮
+    cancel() {
+      this.open = false;
+      this.reset();
+    },
+    // 表单重置
+    reset() {
+      this.form = {
+        id: null,
+        fireId: null,
+        deviceName: null,
+        deviceModel: null,
+        locate: null,
+        alertRecord: null,
+        alertTime: null,
+        createBy: null,
+        createTime: null,
+        updateBy: null,
+        updateTime: null
+      };
+      this.resetForm("form");
+    },
+    /** 搜索按钮操作 */
+    handleQuery() {
+      this.queryParams.pageNum = 1;
+      this.getList();
+    },
+    /** 重置按钮操作 */
+    resetQuery() {
+      this.resetForm("queryForm");
+      this.handleQuery();
+    },
+    // 多选框选中数据
+    handleSelectionChange(selection) {
+      this.ids = selection.map(item => item.id)
+      this.single = selection.length!==1
+      this.multiple = !selection.length
+    },
+    /** 新增按钮操作 */
+    handleAdd() {
+      this.reset();
+      this.open = true;
+      this.title = "添加预警报警";
+    },
+    /** 修改按钮操作 */
+    handleUpdate(row) {
+      this.reset();
+      const id = row.id || this.ids
+      getAlert(id).then(response => {
+        this.form = response.data;
+        this.open = true;
+        this.title = "修改预警报警";
+      });
+    },
+    /** 提交按钮 */
+    submitForm() {
+      this.$refs["form"].validate(valid => {
+        if (valid) {
+          if (this.form.id != null) {
+            updateAlert(this.form).then(response => {
+              this.$modal.msgSuccess("修改成功");
+              this.open = false;
+              this.getList();
+            });
+          } else {
+            addAlert(this.form).then(response => {
+              this.$modal.msgSuccess("新增成功");
+              this.open = false;
+              this.getList();
+            });
+          }
+        }
+      });
+    },
+    /** 删除按钮操作 */
+    handleDelete(row) {
+      const ids = row.id || this.ids;
+      this.$modal.confirm('是否确认删除预警报警编号为"' + ids + '"的数据项？').then(function() {
+        return delAlert(ids);
+      }).then(() => {
+        this.getList();
+        this.$modal.msgSuccess("删除成功");
+      }).catch(() => {});
+    },
+    /** 导出按钮操作 */
+    handleExport() {
+      this.download('alert/alert/export', {
+        ...this.queryParams
+      }, `alert_${new Date().getTime()}.xlsx`)
+    }
+  }
+};
+</script>
